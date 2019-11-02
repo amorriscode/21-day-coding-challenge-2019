4b5b178593ac91421b08a5e12fbb93a43eb6644e
Anthony Morris <anthony@amorrissound.com>
2019-11-02 06:28:19 -0700

    Solve Day 02

---
+++ 02/02.js
@@ -0,0 +1,12 @@
+const interviewAnswer = (topic) => {
+  switch (topic) {
+    case 'arts funding':
+      return 'We\'ll have to get creative!';
+    case 'economy': 
+      return 'Time is money.';
+    case 'transportation':
+      return 'It\'s going to be a long road, so we better get moving.';
+    default:
+      return 'Hmm... I\'ll have to think about that!';
+  }
+}
