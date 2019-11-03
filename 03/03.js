2153dc91cc2624fa94fe713c6acf0e35d2ac9a45
Anthony Morris <anthony@amorrissound.com>
2019-11-03 05:35:02 -0800

    Solve Day 03

---
+++ 03/03.js
@@ -0,0 +1,17 @@
+// Remember, the possible candidates are: Tim, Sally, and Beth (in that order)
+const castVote = (name, votes) => {
+    switch (name) {
+      case "Tim":
+        votes[0]++;
+        break;
+      case "Sally":
+        votes[1]++;
+        break;
+      case "Beth":
+        votes[2]++;
+        break;
+    }
+    
+    return votes;
+  }
+  
